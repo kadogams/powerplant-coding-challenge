6573cd12387b000dee640e9cc60d7caa2f6703f5
skadogam <kadogams@student.s19.be>
2020-05-02 09:23:26 +0200

    add settings.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
+from flask_restful import Api
 
 app = Flask(__name__)
+
+from api.api import PowerplantCodingChallenge
+
+api = Api(app)
+api.add_resource(PowerplantCodingChallenge, '/', '/api')
---
+++ api/settings.py
@@ -0,0 +1,22 @@
+"""
+Settings and global variables for the powerplant-coding-challenge project.
+"""
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+LOGGING_FILE = os.path.join(BASE_DIR, 'powerplant-coding-challenge.log')
+
+MIN_PERCENT = 0
+MAX_PERCENT = 100
+
+MIN_EFFICIENCY = 0
+MAX_EFFICIENCY = 1
+
+FUEL_PARAMS = ['gas(euro/MWh)', 'kerosine(euro/MWh)', 'co2(euro/ton)', 'wind(%)']
+
+POWERPLANT_PARAMS = ['name', 'type', 'efficiency', 'pmin', 'pmax']
+POWERPLANT_TYPES = ['gasfired', 'turbojet', 'windturbine']
+
+ALLOW_FLOAT = True
