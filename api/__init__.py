699ad7cf4465eed4fbf8028498f9c4a03b7a1c57
skadogam <kadogams@student.s19.be>
2020-05-02 15:38:54 +0200

    update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 from api.api import PowerplantCodingChallenge
 
 api = Api(app)
-api.add_resource(PowerplantCodingChallenge, '/', '/api')
+api.add_resource(PowerplantCodingChallenge, '/', '/api/v0')
