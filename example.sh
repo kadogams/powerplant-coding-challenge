35083ff9f99715753f911337e0a2feebbf7469ad
skadogam <kadogams@student.s19.be>
2020-05-03 09:08:09 +0200

    update example.sh

--- example.sh
+++ example.sh
@@ -1,3 +1,3 @@
-curl -X GET http://127.0.0.1:5000/api/v0\
+curl -X POST http://127.0.0.1:5000/api/v0\
     --data "@resource/example_payloads/payload1.json"\
     --header "Content-Type: application/json"
